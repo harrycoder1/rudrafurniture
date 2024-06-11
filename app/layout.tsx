53071031feda5078fa9436b5eae77515e915cf80
harrycoder1 <harishjhedau@gmail.com>
2024-06-11 18:13:21 +0530

    this is my first for test

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Harish Hedau | PortFolio',
+  description: 'This website made by me to represent my self',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head>
+     
+      </head>
+      <body className={`${inter.className}`} >
+
+<div className="mx-[16px] md:mx-[64px] lg:mx-[128px] ">
+{children}
+
+</div>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"  ></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" ></script>
+        {/* <script src="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.jss"></script> */}
+        </body>
+    </html>
+  )
+}
